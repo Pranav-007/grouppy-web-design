f3f014364e754b87e4461ab18bd5f94990b0bc26
Nikhil Gola <nikkstheroc95@gmail.com>
2017-03-10 23:53:00 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+
+    <title>Grouppy</title>
+
+    <?php
+    include 'cdn/CDN.php';
+    ?>
+
+</head>
+
+<body>
+
+<div class="site-wrapper">
+
+    <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand">Grouppy</h3>
+                    <nav>
+                        <ul class="nav masthead-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="inner cover">
+                <h1 class="cover-heading">SEARCH YOUR DOMAIN</h1>
+                <p class="lead">
+
+
+                    <input class="form-control" type="text" id="lg" placeholder="Search for your Group">
+
+
+                <p class="lead">
+                    <a href="#" class="btn btn-lg btn-default"><span class="glyphicon glyphicon-search"></span>Search</a>
+                </p>
+            </div>
+
+            <div class="mastfoot">
+                <div class="inner">
+                    <p><a href="#"> @Grouppy developement Team members</a></p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
